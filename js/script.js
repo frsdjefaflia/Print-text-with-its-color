163a26c33adc7aef405e5439f7e17773a99855a9
frsdjefaflia <djefaflia.fares@gmail.com>
2021-01-31 19:45:17 +0100

    add color to button

--- js/script.js
+++ js/script.js
@@ -14,20 +14,24 @@ myButton.onclick = function (){
     myprint.innerHTML = myInput.value;
     window.myprint.style.color = "#6bb7ec";
     window.mySelect.style.backgroundColor ="#6bb7ec"
+    window.myButton.style.backgroundColor ="#6bb7ec"
   }
   else if (mySelect.value === myRed.value){
     myprint.innerHTML = myInput.value;
     window.myprint.style.color = "#ec6b6b";
     window.mySelect.style.backgroundColor ="#ec6b6b"
+    window.myButton.style.backgroundColor ="#ec6b6b"
   }
   else if (mySelect.value === myYellow.value){
     myprint.innerHTML = myInput.value;
     window.myprint.style.color = "#ecc96b";
     window.mySelect.style.backgroundColor ="#ecc96b"
+    window.myButton.style.backgroundColor ="#ecc96b"
   }
   else if (mySelect.value === myGreen.value){
     myprint.innerHTML = myInput.value;
     window.myprint.style.color = "#85ec6b";
     window.mySelect.style.backgroundColor ="#85ec6b"
+    window.myButton.style.backgroundColor ="#85ec6b"
   }
 };
